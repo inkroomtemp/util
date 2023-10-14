268b077c5e0aa145f85376f985c9a6be1a9fc979
inkroomtemp <125328214+inkroomtemp@users.noreply.github.com>
2023-10-14 23:36:48 +0800

    修改yn

--- yn/yn.py
+++ yn/yn.py
@@ -4,7 +4,7 @@ import os
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.action_chains import ActionChains
-from selenium.webdriver.support import expected_conditions
+from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 from selenium.webdriver.support.ui import WebDriverWait
@@ -39,12 +39,16 @@ class Test1():
     first = self.driver.find_element(By.CSS_SELECTOR, ".upDate:nth-child(1) p")
     actions = ActionChains(self.driver)
     actions.move_to_element(first).click().perform()
+    element = WebDriverWait(self.driver, 50, 0.5).until(EC.presence_of_element_located((By.CSS_SELECTOR, ".avatar")))
+    print("开始睡眠")
+    time.sleep(10)
+    print("睡眠完了")
 
 
     self.vars["win2805"] = self.wait_for_window(2000)
     self.driver.switch_to.window(self.vars["win2805"])
     element = self.driver.find_element(By.CSS_SELECTOR,'body')
-    print('{}'.format(element.text))
+    print('result {}'.format(element.text))
 
 if __name__ == '__main__':
     r = Test1()
